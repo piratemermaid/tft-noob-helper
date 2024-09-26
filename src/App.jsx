2d64a73d3b6b829d7ded6cd5f272dfabbfe17c52
Ariel <thepiratemermaid4@gmail.com>
2024-09-26 13:24:08 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Outlet } from 'react-router-dom';
+import { Box, Button, SimpleGrid } from '@chakra-ui/react';
+
+import NavBar from './components/NavBar';
+import { useStore } from './store';
+import './App.css';
+
+export default function App() {
+  const something = useStore((store) => store.something);
+  const toggleSomething = useStore((store) => store.toggleSomething);
+
+  return (
+    <SimpleGrid columns={1} spacing={4}>
+      <Box>
+        <NavBar />
+      </Box>
+
+      <Box>
+        State thing: {something ? 'yes' : 'no'}{' '}
+        <Button onClick={toggleSomething}>Toggle</Button>
+      </Box>
+
+      <Outlet />
+    </SimpleGrid>
+  );
+}
