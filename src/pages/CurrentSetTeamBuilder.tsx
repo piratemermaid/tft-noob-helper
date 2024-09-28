1f959b6c5455a3f680bb82f53776353c4fe5fed9
Ariel <thepiratemermaid4@gmail.com>
2024-09-28 12:30:44 -0600

    clean up old code

--- src/pages/CurrentSetTeamBuilder.tsx
+++ src/pages/CurrentSetTeamBuilder.tsx
@@ -2,18 +2,13 @@ import {
   Box,
   Checkbox,
   CheckboxGroup,
-  Grid,
   GridItem,
   Input,
-  ListItem,
   SimpleGrid,
-  Spacer,
   Text,
-  UnorderedList,
   VStack,
 } from '@chakra-ui/react';
 
-import ChampionImage from '../components/images/ChampionImage';
 import ChampionList from '../components/ChampionList';
 import PageLayout from '../components/PageLayout';
 import CURRENT_CHAMPIONS from '../data/currentChampions';
@@ -41,33 +36,6 @@ export default function CurrentSetTeamBuilder() {
           </VStack>
         </GridItem>
       </SimpleGrid>
-    </PageLayout>
-  );
-
-  return (
-    <PageLayout title="Current Set Team Builder">
-      <Grid templateColumns={'repeat(3, 1fr)'}>
-        <GridItem>TEAM BUILDER</GridItem>
-        <Spacer />
-        <GridItem>
-          <VStack>
-            {CURRENT_CHAMPIONS.map((champion) => (
-              <SimpleGrid key={champion.name} columns={4} spacing={1}>
-                <Text>{champion.name}</Text>
-                <Text>{champion.cost}</Text>
-                <ChampionImage name={champion.name} />
-                <UnorderedList>
-                  {champion.classes.map((trait) => (
-                    <ListItem key={trait}>
-                      <Text>{trait}</Text>
-                    </ListItem>
-                  ))}
-                </UnorderedList>
-              </SimpleGrid>
-            ))}
-          </VStack>
-        </GridItem>
-      </Grid>
     </PageLayout>
   );
 }
