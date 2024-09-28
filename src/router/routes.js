dd74ab21b3fb437ca3bcdf91bcdb4099ed42f92f
Ariel <thepiratemermaid4@gmail.com>
2024-09-28 11:01:30 -0600

    remove placeholder pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,10 @@
 import TraitOverlap from '../pages/TraitOverlap';
-import PageOne from '../pages/PageOne';
-import PageTwo from '../pages/PageTwo';
 
 const ROUTES = [
   {
     label: 'Trait Overlap',
     path: '/',
     Component: TraitOverlap,
-  },
-  {
-    label: 'Page One',
-    path: '/one',
-    Component: PageOne,
-  },
-  {
-    label: 'Page Two',
-    path: '/two',
-    Component: PageTwo,
   },
 ];
 
