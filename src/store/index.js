3433da1b9eb57eaf5e41046a03867589b7ae814a
Ariel <thepiratemermaid4@gmail.com>
2024-09-28 13:05:19 -0600

    remove testing logs

--- src/store/index.js
+++ src/store/index.js
@@ -6,18 +6,15 @@ export const useStore = create(
     (set, get) => ({
       selectedChampions: [],
       handleSelectChampion: (champion) => {
-        console.log('>>', champion);
         const selectedChampions = get().selectedChampions;
 
         if (selectedChampions.includes(champion)) {
-          console.log('>> remove:', champion);
           const newSelectedChampions = selectedChampions.filter(
             (selectedChamp) => selectedChamp !== champion
           );
           return set({ selectedChampions: newSelectedChampions });
         }
 
-        console.log('>> add:', champion);
         return set({ selectedChampions: [...selectedChampions, champion] });
       },
     }),
