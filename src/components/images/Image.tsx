2e622dabc573c2e36d98875349e3b6bfb67ad0db
Ariel <thepiratemermaid4@gmail.com>
2024-09-28 15:44:51 -0600

    update image code to remove periods

--- src/components/images/Image.tsx
+++ src/components/images/Image.tsx
@@ -1,12 +1,15 @@
 import { Image as ChakraImage } from '@chakra-ui/react';
 
 export default function Image({ name, ...rest }) {
+  const formattedName = name
+    .replaceAll(' ', '')
+    .replace('&', '')
+    .replaceAll('.', '')
+    .replace("'", '');
+
   return (
     <ChakraImage
-      src={`/images/${name
-        .replaceAll(' ', '')
-        .replace('&', '')
-        .replace("'", '')}.png`}
+      src={`/images/${formattedName}.png`}
       title={name}
       alt={name}
       sx={{ width: '40px', height: '40px' }}
