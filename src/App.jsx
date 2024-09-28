f0cac1b9e999cba0f5d01bea9fa6e8d79130f708
Ariel <thepiratemermaid4@gmail.com>
2024-09-28 16:29:59 -0600

    clean up boilerplate removal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import { Box, Button, SimpleGrid } from '@chakra-ui/react';
+import { Box, SimpleGrid } from '@chakra-ui/react';
 
 import NavBar from './components/NavBar';
-import { useStore } from './store';
 import './App.css';
 
 export default function App() {
-  const something = useStore((store) => store.something);
-  const toggleSomething = useStore((store) => store.toggleSomething);
-
   return (
     <SimpleGrid columns={1} spacing={4}>
       <Box>
