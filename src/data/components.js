8a4787181ce5cccf1e90e9563caa47ec2a7b3ca7
Ariel <thepiratemermaid4@gmail.com>
2024-09-28 19:19:11 -0600

    reorder components

--- src/data/components.js
+++ src/data/components.js
@@ -1,14 +1,14 @@
 const ITEM_COMPONENTS = [
   { name: 'B.F. Sword' },
   { name: 'Chain Vest' },
-  { name: 'Frying Pan' },
   { name: "Giant's Belt" },
   { name: 'Needlessly Large Rod' },
   { name: 'Negatron Cloak' },
   { name: 'Recurve Bow' },
   { name: 'Sparring Gloves' },
-  { name: 'Spatula' },
   { name: 'Tear of the Goddess' },
+  { name: 'Spatula' },
+  { name: 'Frying Pan' },
 ];
 
 export default ITEM_COMPONENTS;
