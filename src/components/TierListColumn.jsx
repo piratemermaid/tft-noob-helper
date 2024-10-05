2fd381492633871b458333a9b46eb15a2b5c3a8e
Ariel <thepiratemermaid4@gmail.com>
2024-10-05 12:33:42 -0600

    add my champs and bold them

--- src/components/TierListColumn.jsx
+++ src/components/TierListColumn.jsx
@@ -1,7 +1,8 @@
 import _ from 'lodash';
-
-import { Text, VStack } from '@chakra-ui/react';
 import { Fragment } from 'react';
+import { Text, VStack } from '@chakra-ui/react';
+
+import MY_CHAMPS from '../data/myChamps';
 
 export default function TierListColumn({ title, tierList }) {
   const tierColors = {
@@ -17,7 +18,14 @@ export default function TierListColumn({ title, tierList }) {
         {_.map(tierList, (champions, tier) => (
           <Fragment key={tier}>
             {champions.map((champion) => (
-              <Text key={champion} sx={{ color: tierColors[tier], my: -1 }}>
+              <Text
+                key={champion}
+                sx={{
+                  color: tierColors[tier],
+                  my: -1,
+                  fontWeight: MY_CHAMPS.includes(champion) ? 'bold' : 'normal',
+                }}
+              >
                 {champion}
               </Text>
             ))}
---
+++ src/data/myChamps.js
@@ -0,0 +1,23 @@
+// TODO: Put in store before sharing app
+const MY_CHAMPS = [
+  'Annie',
+  'Ashe',
+  'Aurelion Sol',
+  'Caitlyn',
+  'Camille',
+  'Garen',
+  'Lux',
+  'Miss Fortune',
+  'Morgana',
+  'Poppy',
+  'Pyke',
+  'Senna',
+  'Sivir',
+  'Teemo',
+  'Tristana',
+  'Vayne',
+  'Veigar',
+  'Yuumi',
+];
+
+export default MY_CHAMPS;
