026ac01b82eaf0d67eaba18ed536f78a22903638
Ariel <thepiratemermaid4@gmail.com>
2024-10-25 14:49:03 -0600

    make cards use actual card component

--- src/components/ChampCheatsheet/CheatsheetCardUL.jsx
+++ src/components/ChampCheatsheet/CheatsheetCardUL.jsx
@@ -1,16 +1,18 @@
-import { ListItem, UnorderedList, VStack } from '@chakra-ui/react';
+import { Card, ListItem, UnorderedList, VStack } from '@chakra-ui/react';
 
 import { SubHeader } from './Headers';
 
 export default function CheatsheetCardUL({ title, items }) {
   return (
-    <VStack sx={{ border: '1px solid gray', borderRadius: 15, p: 3, pt: 1 }}>
-      <SubHeader>{title}</SubHeader>
-      <UnorderedList sx={{ textAlign: 'left' }}>
-        {items.map((item, index) => (
-          <ListItem key={index}>{item}</ListItem>
-        ))}
-      </UnorderedList>
-    </VStack>
+    <Card sx={{ border: '1px solid gray', borderRadius: 15, p: 3, pt: 1 }}>
+      <VStack>
+        <SubHeader>{title}</SubHeader>
+        <UnorderedList sx={{ textAlign: 'left' }}>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </UnorderedList>
+      </VStack>
+    </Card>
   );
 }
