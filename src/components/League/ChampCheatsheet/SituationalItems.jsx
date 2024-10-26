59444d5bb7c2e8867048d76776a3bb20cc64881f
Ariel <thepiratemermaid4@gmail.com>
2024-10-26 10:08:31 -0600

    fix lint warnings

--- src/components/League/ChampCheatsheet/SituationalItems.jsx
+++ src/components/League/ChampCheatsheet/SituationalItems.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, GridItem, SimpleGrid, Text } from '@chakra-ui/react';
+import { Grid, GridItem, Text } from '@chakra-ui/react';
 import Image from '../../images/Image';
 
 export default function SituationalItems({ cheatsheet }) {
@@ -8,6 +8,7 @@ export default function SituationalItems({ cheatsheet }) {
     <>
       {cheatsheet.situationalItems.map((item) => (
         <Grid
+          key={item.name}
           templateColumns="repeat(4, 1fr)"
           gap={1}
           sx={{ border: '1px solid gray', p: 2 }}
@@ -32,36 +33,6 @@ export default function SituationalItems({ cheatsheet }) {
             <Text>{item.alternativeFor}</Text>
           </GridItem>
         </Grid>
-      ))}
-    </>
-  );
-  return (
-    <>
-      {cheatsheet.situationalItems.map((item) => (
-        <SimpleGrid columns={3} spacing={0}>
-          <GridItem sx={{ border: '1px solid blue' }}>
-            <Image
-              name={item.name}
-              type="leagueItem"
-              width="20px"
-              height="20px"
-            />
-            <Text sx={{ position: 'relative', top: 0, right: 0 }}>
-              {item.name}
-            </Text>
-          </GridItem>
-          {/* <GridItem sx={{ width: 'auto', border: '1px solid blue' }}>
-            <Text>{item.name}</Text>
-            <Text>({item.alternativeFor} alternative)</Text>
-            </GridItem> */}
-          <GridItem sx={{ border: '1px solid blue' }}>
-            <Text>{item.summary}</Text>
-          </GridItem>
-          <GridItem sx={{ border: '1px solid blue' }}>
-            <Text>Replaces:</Text>
-            <Text>{item.alternativeFor}</Text>
-          </GridItem>
-        </SimpleGrid>
       ))}
     </>
   );
